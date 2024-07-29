6c5c6e47fab1de61a01b10e55e956ad8af9c335b
HARISARAN405 <113167317+HARISARAN405@users.noreply.github.com>
2024-07-29 16:28:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+function calculateTip() {
+    var billAmount = document.getElementById("billAmount").value;
+    var tipPercentage = document.getElementById("tipPercentage").value;
+  
+    if (billAmount === "" || tipPercentage === "") {
+      alert("Please enter both bill amount and tip percentage.");
+      return;
+    }
+  
+    var tipAmount = (billAmount * tipPercentage) / 100;
+    var totalAmount = parseFloat(billAmount) + parseFloat(tipAmount);
+  
+    document.getElementById("totalDisplay").innerText = "Total: $" + totalAmount.toFixed(2);
+  }
+  
